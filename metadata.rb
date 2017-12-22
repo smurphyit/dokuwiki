c7e9e88407076865064091c0d3aec4fde2a6bf8a
smurphy.it <smurphy.it@gmail.com>
2017-12-22 19:14:03 -0400

    update dokuwiki recipe

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,6 @@ version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 depends 'tar', '~> 2.1.1'
 
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- recipes/dokuwiki.rb
+++ recipes/dokuwiki.rb
@@ -4,17 +4,21 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 ## Create the destination directory
-directory '/var/www/tha.local/public_html/' do
+directory '/var/www/tha.local/public_html/mycms' do
   owner 'www-data'
   group 'www-data'
-  mode '0755'
+  mode '0700'
   action :create
   recursive true
 end
 
 ### Tarball command depends on the tarball cookbook, this should be a requirement under metadata.rb ###
 tar_extract 'http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz' do
+  group 'www-data'
+  user 'www-data'
   action :extract
-  target_dir '/var/www/tha.local/public_html/'
+  target_dir '/var/www/tha.local/public_html/mycms'
+  tar_flags [ '--strip-components 1' ]  
+  creates '/var/www/tha.local/public_html/mycms/COPYING'
 end
 
